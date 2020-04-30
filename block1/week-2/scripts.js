5379b182034818ad6b2fa958fc3f96b00bffb66f
shakerbaker78 <bakeamym@yahoo.com>
2020-04-30 16:18:19 -0600

    quiz

---
+++ block1/week-2/scripts.js
@@ -0,0 +1,12 @@
+const heroes = [];
+heroes[0] = `Superman`;
+heroes[1] = `Wonder Woman`;
+heroes[2] = `Flash`;
+heroes[3] = `Batman`;
+heroes[5] = `Aquaman`;
+console.log(heroes[0]);
+console.log(heroes)
+
+
+const avengers = ['Captain America', 'Iron Man', 'Thor', 'Hulk'];
+console.log(avengers);
