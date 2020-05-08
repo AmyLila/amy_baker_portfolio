07965cd7d9e686b66a107b19dc086cd26bb9d2a6
shakerbaker78 <bakeamym@yahoo.com>
2020-05-07 19:59:35 -0600

    chapter 5

--- block1/week-3/scripts.js
+++ block1/week-3/scripts.js
@@ -1,5 +1,51 @@
+// Chapter 5 Code Example
+
+      const quiz = [
+          { name: "Superman",realName: "Clark Kent" },
+          { name: "Wonder Woman",realName: "Diana Prince" },
+          { name: "Batman",realName: "Bruce Wayne" },
+      ];
+  
+      const game = {
+          start(quiz){
+              this.questions = [...quiz];
+              this.score = 0;
+              // main game loop
+              for(const question of this.questions){
+              this.question = question;
+              this.ask();
+              }
+              // end of main game loop
+              this.gameOver();
+          },
+          ask(){
+              const question = `What is ${this.question.name}'s real name?`;
+              const response =  prompt(question);
+              this.check(response);
+          },
+          check(response){
+              const answer = this.question.realName;
+              if(response === answer){
+              alert('Correct!');
+              this.score++;
+              } else {
+              alert(`Wrong! The correct answer was ${answer}`);
+              }
+          },
+          gameOver(){
+              alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+          }
+      }
+
+  
+
+
+
+
+
+// Callback NASA example
 var Apod_URL = 'https://api.nasa.gov/planetary/apod';
-var apiKey = 'yeVL7qV2WbHyqLfR0HV3AZrEW57kcFuyH2SmiifQ'
+var apiKey = 'DHWf9NoDJKU9m25RKvQoNiv7ftd9jDzJsWVBVkdV'
 
 function helloAPOD() {
      var request = new XMLHttpRequest();
