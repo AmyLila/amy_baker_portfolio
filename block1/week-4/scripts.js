bd38244d7b7deebd49bf034319408eebf078d6c9
shakerbaker78 <bakeamym@yahoo.com>
2020-05-12 23:04:33 -0600

    halfway through chapter 8

--- block1/week-4/scripts.js
+++ block1/week-4/scripts.js
@@ -1,216 +1,90 @@
-// Chapter 5 Code Example
+// Chapter 8 Code Example
+//Accessing Form Elements
+//const form = document.forms[0];
+const form = document.forms['search'];
+//Accessing the search input bar
+const input = form.elements.searchInput;
 
-const quiz = [
-    { name: "Superman",realName: "Clark Kent" },
-    { name: "Wonder Woman",realName: "Diana Prince" },
-    { name: "Batman",realName: "Bruce Wayne" },
-];
-  
-const game = {
-    start(quiz){
-        this.questions = [...quiz];
-        this.score = 0;
-        // main game loop
-        for(const question of this.questions){
-        this.question = question;
-        this.ask();
-        }
-        // end of main game loop
-        this.gameOver();
-    },
-    ask(){
-        const question = `What is ${this.question.name}'s real name?`;
-        const response =  prompt(question);
-        this.check(response);
-    },
-    check(response){
-        const answer = this.question.realName;
-        if(response === answer){
-        alert('Correct!');
-        this.score++;
-        } else {
-        alert(`Wrong! The correct answer was ${answer}`);
-        }
-    },
-    gameOver(){
-        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
-    }
+//Events
+//Focus Event
+//input.addEventListener('focus', () => alert('focused'), false);
+
+// Blur Event
+// input.addEventListener('blur', () => alert('blurred'), false);
+
+//Change Event
+input.addEventListener('change', () => alert('changed'), false);
+
+// Submit Event
+form.addEventListener ('submit', search, false);
+// function search() {
+//     alert(' Form Submitted');
+// }
+//Submit Event and prevent default
+// function search(event) {
+//     alert('Form Submitted');
+//     event.preventDefault();
+// }
+
+
+//Retrieving and Changing Values From a Form
+//Reports back what the user searched for
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
 }
 
-// Chapter 6 and 7 Quiz Ninja Project
-// View Object
-const view = {
-    score: document.querySelector('#score strong'),
-    question: document.getElementById('question'),
-    result: document.getElementById('result'),
-    info: document.getElementById('info'),
-    render(target,content,attributes) {
-        for(const key in attributes) {
-            target.setAttribute(key, attributes[key]);
-        }
-        target.innerHTML = content;
-    },
-    start: document.getElementById('start'),
-    show(element){
-        element.style.display = 'block';
-    },
-    hide(element){
-        element.style.display = 'none';
-    }
-};
+// Set Value using JavaScript
+input.value = 'Search Here';
 
-const game2 = {
-    start(quiz){
-        this.questions = [...quiz];
-        this.score = 0;
-        // main game loop
-        for(const question of this.questions){
-        this.question = question;
-        this.ask();
-        view.hide(view.start);
-        }
-        // end of main game loop
-        this.gameOver();
-    },
-    ask(){
-        const question = `What is ${this.question.name}'s real name?`;
-        view.render(view.question,question);
-        const response =  prompt(question);
-        this.check(response);
-    },
-    check(response){
-        const answer = this.question.realName;
-        if(response === answer){
-        view.render(view.result,'Correct!',{'class':'correct'});
-        alert('Correct!');
-        this.score++;
-        view.render(view.score,this.score);
-        } else {
-        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
-        alert(`Wrong! The correct answer was ${answer}`);
-        }
-    },
-    gameOver(){
-        view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
-        view.show(view.start);
+// Make the text disapear when focused on
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
     }
+}, false); // What does this "false" do?
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
 
+
+//Form Controls
+const heroForm = document.forms['hero'];
+heroForm.addEventListener('submit', makeHero, false);
+
+function makeHero(event) {
+    event.preventDefault(); // prevent the form from being submitted
+    const hero = {}; // create an empty object
+    hero.name = orm.heroName.value; // create a name property based on the input field's value
+    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+
+    hero.realName = form.realName.value; //Access the password field
+
+    // Creates an empty array called powers and adds powers that are checked to the powers array
+//     hero.powers = [];
+//     for (let i=0; i < form.powers.length; i++) {
+//         if (form.powers[i].checked) {
+//          hero.powers.push(form.powers[i].value);
+//     }
+// }
+    //This is the same code refactored
+    hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value);
+
+    return hero;
 }
-view.start.addEventListener('click', () => game2.start(quiz), false);
+
+
+
+
+
+// Chapter 6 Code Example
+
+
 
 
 //Chapter 7
-//This function tells you where the mouse was when it clicked
-// function doSomething(event){
-//     console.log(`screen: (${event.screenX},${event.screenY}), page: (${event.pageX},${event.pageY}), client: (${event.screenX},${event.screenY})`)
-// }
-// addEventListener('click', doSomething);
 
-//Mouse event examples
-const clickParagraph = document.getElementById('click');
-clickParagraph.addEventListener('click',() => console.log('click') );
-clickParagraph.addEventListener('mousedown',() => console.log('down') );
-clickParagraph.addEventListener('mouseup',() => console.log('up') );
-
-const dblclickParagraph = document.getElementById('dblclick');
-dblclickParagraph.addEventListener('click', highlight);
-function highlight(event){
-    event.target.classList.toggle('highlight');
-}
-
-const mouseParagraph = document.getElementById('mouse');
-mouseParagraph.addEventListener('mouseover', highlight);
-mouseParagraph.addEventListener('mouseout', highlight);
-mouseParagraph.addEventListener('mousemove', () =>  console.log('You Moved!') );
-
-// Keyboard events
-//addEventListener('keydown',highlight);
-addEventListener('keyup', (event) => console.log(`You stopped pressing the key on ${new Date}`));
-addEventListener('keypress', (event) => console.log(`You pressed the ${event.key} character`));
-//modifier keys
-addEventListener('keydown', (event) => console.log(`You pressed the ${event.key} character`));
-addEventListener('keydown', (event) => {
-    if (event.key === 'c' && event.ctrlKey) {
-            console.log('Action canceled!');
-        }
-    });
-addEventListener('click', (event) => {
-    if (event.shiftKey) {
-        console.log('A Shifty Click!');
-     }
-});    
-
-// Removing Event Listeners
-const onceParagraph = document.getElementById('once');
-onceParagraph.addEventListener('click', remove);
-function remove(event) {
-    console.log('Enjoy this while it lasts!');
-    onceParagraph.style.backgroundColor = 'pink';
-    onceParagraph.removeEventListener('click',remove);
-}
-
-//Stopping Default Behavior
-const brokenLink = document.getElementById('broken');
-brokenLink.addEventListener('click',(event) => {
-    event.preventDefault();
-    console.log('Broken Link!');
-});
-
-//Event Propagation
-// capturing
-ulElement.addEventListener('click', (event) =>
-console.log('Clicked on ul'),true);
-liElement.addEventListener('click', (event) =>
-console.log('Clicked on li'),true);
-
-// bubbling
-ulElement.addEventListener('click', (event) =>
-console.log('Clicked on ul'),false );
-liElement.addEventListener('click', (event) =>
-console.log('Clicked on li'),false );
-
-//Event Delegation
-const delExe = document.getElementById('list1');
-delExe.addEventListener('click',highlight);
-
-
-
-
-
-
-// Callback NASA example
-var Apod_URL = 'https://api.nasa.gov/planetary/apod';
-var apiKey = 'DHWf9NoDJKU9m25RKvQoNiv7ftd9jDzJsWVBVkdV'
-
-function helloAPOD() {
-     var request = new XMLHttpRequest();
-     request.open('GET', Apod_URL + '?api_key=' + apiKey, true);
-
-     request.addEventListener('load',() => {
-           if(request.status >= 200 && request.status < 400) {
-                var response = JSON.parse(request.responseText);
-                 console.log(response);
-                 document.getElementById('original').innerHTML = response.explanation;
-                 document.getElementById('originalImg').src = response.url;
-           }
-          else {
-             console.log("Error in network request: " + request.statusText);
-          }
-    }
-   );
-     request.send(null);
-}
-
-function helloAPODV2() {
-  fetch(    Apod_URL + '?api_key=' + apiKey,     {method: 'GET'}   )
-    .then(response => response.json())
-    .then(json => {
-      document.getElementById('fetch').innerHTML = json.explanation;
-      document.getElementById('fetchImg').src = json.url;
-      console.log(json)
-    })
-    .catch(error => console.error('error:', error));
-}
 
 WebFont.load({
     google: {
