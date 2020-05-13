3885fb7bb0870c7426c846027c7aa4ccfafcf5ad
shakerbaker78 <bakeamym@yahoo.com>
2020-05-12 23:30:37 -0600

    f

--- block1/week-4/scripts.js
+++ block1/week-4/scripts.js
@@ -70,6 +70,12 @@ function makeHero(event) {
 // }
     //This is the same code refactored
     hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value);
+    // "This uses the spread operator to turn the node list into an array. This then allows us to use the filter() method that returns an array containing only the check boxes that were checked (this is because their 'checked' property will be truthy). We then chain the map() method to the end, which replaces each checkbox in the array with its 'value' property. This array is then returned and stored in the hero.powers variable."
+
+    hero.category = form.category.value; // Set the radio button value
+    hero.age = form.age.value; // Set the age number input
+    hero.city = form.city.value; //set the city property:
+    hero.origin = form.origin.value; // Set the origin property
 
     return hero;
 }
