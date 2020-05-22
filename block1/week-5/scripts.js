ea3a6c969a39768717545e51499a3d79de178441
shakerbaker78 <bakeamym@yahoo.com>
2020-05-22 14:42:39 -0600

    finished week 5

--- block1/week-5/scripts.js
+++ block1/week-5/scripts.js
@@ -1,3 +1,59 @@
+'use strict';
+// code examples
+// function amIOldEnough(age){
+//   if (age < 12) {
+//       alert(age);
+//       return 'No, sorry.';
+//   } else if (age < 18) {
+//       return 'Only if you are accompanied by an adult.';
+//   }
+//   else {
+//       return 'Yep, come on in!';
+//   }
+// }
+
+function amIOldEnough(age){
+  console.log(age);
+      if (age < 12) {
+      console.log(`In the if with ${age}`);
+      return 'No, sorry.';
+      } else if (age < 18) {
+      console.log(`In the else-if with ${age}`);
+      return 'Only if you are accompanied by an adult.';
+      } else {
+      console.log(`In the else with ${age}`);
+      return 'Yep, come on in!';
+  }
+}
+
+console.log(amIOldEnough(21));
+
+// Throw example
+function squareRoot(number) {
+  'use strict';
+  if (number < 0) {
+      throw new RangeError('You can not find the square root of negative numbers')
+    }
+  return Math.sqrt(number);
+};
+console.log(squareRoot(121));
+
+//console.log(squareRoot(-1));
+
+// try block example
+function imaginarySquareRoot(number) {
+  'use strict';
+  let answer;
+  try {
+      answer = String(squareRoot(number));
+  } catch(error) {
+      answer = squareRoot(-number)+"i";
+  } finally {
+      return `+ or - ${answer}`;
+  }
+}
+console.log(imaginarySquareRoot(-49));
+
 
 // Quiz Ninja Code
 const quiz = [
@@ -97,6 +153,14 @@ const game = {
 view.start.addEventListener('click', () => game.start(quiz), false);
 view.response.addEventListener('submit', (event) => game.check(event), false);
 view.hide(view.response);
+
+
+// NEW QUIZ NINJA CODE!!!!!!!!
+console.log('start() invoked');
+console.log('ask() invoked');
+console.log('check(event) invoked');
+console.log('gameOver() invoked');
+
 
 // end quiz Ninja!
 
