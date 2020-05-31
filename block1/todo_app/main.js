32ec803850242eed02da9111ef81530804f168d0
shakerbaker78 <bakeamym@yahoo.com>
2020-05-30 18:16:04 -0600

    js

--- block1/todo_app/main.js
+++ block1/todo_app/main.js
@@ -1,5 +1,5 @@
 // Imports
-//import * as fonts from './webfonts.js';
+import * as test from './todo.js';
 import { toDoList, toDoForm, tasks, unfinishedList, finishedList, submitTask,displayTasks, deleteItem, completedTasks, filterFinished, filterNotFinished} from './to_do.js';
 import { saveToLs, getTasks } from './ls.js';
 import * as util from './utilities.js';
