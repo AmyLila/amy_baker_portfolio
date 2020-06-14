83f6dc11933ae236caf136175bb023d424f3dd93
shakerbaker78 <bakeamym@yahoo.com>
2020-06-13 22:10:00 -0600

    transitions finished

--- block2/week-1/scripts.js
+++ block2/week-1/scripts.js
@@ -1,4 +1,21 @@
-'use strict';
+window.onload = function() {
+  var transition1 = document.getElementById('transition1');
+  var transition2 = document.getElementById('transition2');
+  var transition3 = document.getElementById('transition3');
+  transition1.addEventListener('click', changeClass, false);
+  transition2.addEventListener('click', changeClass, false);
+  transition3.addEventListener('click', changeClass, false);
+}
+
+function changeClass(){
+  if (this.className == 'move'){
+    this.className = 'still';
+    
+  }else if (this.className == 'still'){
+    this.className = 'move';
+    
+  }
+}
 
 
 WebFont.load({
