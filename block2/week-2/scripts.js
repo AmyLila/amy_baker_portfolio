a3f8b258c0df6a2b7bf368afd82006b669acce62
shakerbaker78 <bakeamym@yahoo.com>
2020-06-20 22:07:38 -0600

    week 9

--- block2/week-2/scripts.js
+++ block2/week-2/scripts.js
@@ -1,55 +1,161 @@
 // Chapter 8
-window.onload = function() {
-  var transition1 = document.getElementById('transition1');
-  var transition2 = document.getElementById('transition2');
-  var transition3 = document.getElementById('transition3');
-  transition1.addEventListener('click', changeClass, false);
-  transition2.addEventListener('click', changeClass, false);
-  transition3.addEventListener('click', changeClass, false);
+
+// Animation example from chapter 9
+const squareElement = document.getElementById('square');
+let angle = 0;
+setInterval( () => {
+    angle = (angle + 2) % 360;
+    squareElement.style.transform = `rotate(${angle}deg)`
+}, 1000/60);
+
+//Request Animation Frame from chapter 9
+const squareElement1 = document.getElementById('square1');
+function rotate() {
+    angle = (angle + 2)%360;
+    squareElement1.style.transform = `rotate(${angle}deg)`
+    window.requestAnimationFrame(rotate);
+}
+const id = requestAnimationFrame(rotate);
+
+
+
+//Quiz Ninja from chapter 9 and 14
+
+const url = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/questions.json';
+
+fetch(url)
+  .then(res => res.json())
+  .then(quiz => {
+    view.start.addEventListener('click', () => game.start(quiz.questions), false);
+    view.response.addEventListener('click', (event) => game.check(event), false);
+});
+
+//Function examples from chapter 11
+// Utility functions
+function random(a,b=1) {
+// if only 1 argument is provided, we need to swap the values of a and b
+  if (b === 1) {
+	  [a,b] = [b,a];
+  }
+  return Math.floor((b-a+1) * Math.random()) + a;
 }
 
-function changeClass(){
-  if (this.className == 'move'){
-    this.className = 'still';
-    
-  }else if (this.className == 'still'){
-    this.className = 'move';
-    
+function shuffle(array) {
+  for (let i = array.length; i; i--) {
+      let j = random(i)-1;
+      [array[i - 1], array[j]] = [array[j], array[i - 1]];
   }
 }
 
-// Chapter 12 examples
-//Canvas
-//"On a real-life painting canvas, you must first saturate your brush with paint before you can begin. In the HTML5 canvas, you must do the same, and we do so with the strokeStyle or fillStyle properties. Both strokeStyle and fillStyle are set on a context object, and both take one of three values: a string representing a color, a CanvasGradient object, or a CanvasPattern object."
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
-context.strokeStyle = "rgb(128,0,128)";
-context.fillStyle = "rgba(128,0,128, 0.5)";
-context.fillRect(10, 10, 100, 100);   
-context.strokeRect(10, 10, 100, 100);
+// View Object
+const view = {
+  score: document.querySelector('#score strong'),
+  question: document.querySelector('#question'),
+  result: document.querySelector('#result'),
+  info: document.querySelector('#info'),
+  start: document.querySelector('#start'),
+  response: document.querySelector('#response'),
+  timer: document.querySelector('#timer strong'),
+  render(target,content,attributes) {
+      for(const key in attributes) {
+        target.setAttribute(key, attributes[key]);
+      }
+      target.innerHTML = content;
+  },
+  show(element){
+    element.style.display = 'block';
+  },
+  hide(element){
+    element.style.display = 'none';
+  },
+  setup(){
+    this.show(this.question);
+    this.show(this.response);
+    this.show(this.result);
+    this.hide(this.start);
+    this.render(this.score,game.score);
+    this.render(this.result,'');
+    this.render(this.info,'');
+    this.render(this.hiScore, game.hiScore());
+  },
+  teardown(){
+    this.hide(this.question);
+    this.hide(this.response);
+    this.show(this.start);
+    this.render(this.hiScore, game.hiScore());
+  },
+  buttons(array){
+    return array.map(value => `<button>${value}</button>`).join('');
+  }
+};
 
-//Gradient example
-var canvas = document.getElementById("myCanvas2");
-var context = canvas.getContext("2d");
-var gradient = context.createLinearGradient(0, 0, 0, 200);
-gradient.addColorStop(0, "blue"); 
-gradient.addColorStop(1, "white"); 
-context.fillStyle = gradient; 
-context.fillRect(10, 10, 100, 100); 
-context.strokeRect(10, 10, 100, 100); 
+const game = {
+  start(quiz){
+    console.log('start() invoked');
+    this.score = 0;
+    this.questions = [...quiz];
+    view.setup();
+    this.secondsRemaining = 20;
+    this.timer = setInterval( this.countdown , 1000 );
+    this.ask();
+  },
+  countdown() {
+    game.secondsRemaining--;
+    view.render(view.timer,game.secondsRemaining);
+      if(game.secondsRemaining <= 0) {
+        game.gameOver();
+      }
+  },
+  ask(name){
+    console.log('ask() invoked');
+    if(this.questions.length > 2) {
+      shuffle(this.questions);
+      this.question = this.questions.pop();
+      const options = [this.questions[0].realName, this.questions[1].realName, this.question.realName];
+      shuffle(options);
+      const question = `What is ${this.question.name}'s real name?`;
+      view.render(view.question,question);
+      view.render(view.response,view.buttons(options));
+    }
+    else {
+      this.gameOver();
+    }
+  },
+  check(event){
+    console.log('check(event) invoked');
+    const response = event.target.textContent;
+    const answer = this.question.realName;
+    if(response === answer){
+      console.log('correct');
+      view.render(view.result,'Correct!',{'class':'correct'});
+      this.score++;
+      view.render(view.score,this.score);
+    } else {
+      console.log('wrong');
+      view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+    }
+    this.ask();
+  },
+  gameOver(){
+    console.log('gameOver() invoked');
+    view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+    view.teardown();
+    clearInterval(this.timer);
+  },
 
-//circle example
-var canvas = document.getElementById("myCanvasCircle");
-var context = canvas.getContext("2d");
-context.beginPath();
-context.arc(50, 50, 30, 0, Math.PI*2, true);
-context.closePath(); 
-context.lineWidth = 3; 
-context.strokeStyle = "rgb(128,0,128)";
-context.fillStyle = "rgba(128,0,128, 0.5)";
-context.lineWidth = 3;
-context.fill(); 
-context.stroke();
+
+  hiScore(){
+    const hi = localStorage.getItem('highScore') || 0;
+    if(this.score > hi || hi === 0) {
+      localStorage.setItem('highScore',this.score);
+      view.render(view.info,'** NEW HIGH SCORE! **');
+    }
+    return localStorage.getItem('highScore');
+  }
+}
+
+// end quiz Ninja!
+
 
 WebFont.load({
     google: {
