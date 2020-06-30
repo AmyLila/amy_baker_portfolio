d661c15297461cb09ae1fd3224b3eadf70b6443d
shakerbaker78 <bakeamym@yahoo.com>
2020-06-29 20:48:57 -0600

    images

--- block2/monster_faces/scripts/monster.js
+++ block2/monster_faces/scripts/monster.js
@@ -1,27 +1,36 @@
+const { yellow } = require("color-name");
+
 // get monster info from the JSON file
 var monsterURL = 'https://shakerbaker78.github.io./amy_baker_portfolio/block2/monster_faces/data/monster.json';
-
+let createMonster = document.getElementById("makeMonster");
+//createMonster.addEventListener("click", makeMonster, false)
+createMonster.addEventListener("click", () => makeMonster(yellow, 1));
 console.log("I am working")
 
-function makeMonster() {
+//To Do: Make a color argument and change the event listener so it works
+function makeMonster(color, face_number) {
     console.log("Monster Function Working")
-  fetch(monsterURL,{method: 'GET'})
+    fetch(monsterURL, {method: 'GET'})
     .then(response => response.json())
     .then(json => {
-        console.log(jsonObject); //temporary checking for valid response and data parsing
-        const monster = jsonObject['monsters'];
+        console.log(json);
+        const monster = json['monsters'];
+        console.log(monster)
         for (let i = 0; i < monster.length; i++ ) {
-            if(monster[i].color == "green"){
-                console.log("Green Working")
+            if (monster[i].color == "yellow") {
+                console.log("yellow works")
+                
+                let monsterImage = document.createElement('img');
+                monsterImage.setAttribute('src', monster[i].body);
+                monsterImage.setAttribute('alt', monster[i].color);
 
+                document.querySelector('.results').appendChild(monsterImage);
+                
             }
             
-
         }
-        
+    })
 
-    })   
-    .catch(error => console.error('error:', error));
+
 }
-
 
